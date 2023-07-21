e115f6c8c8c47b14870becdf86b1dcd046c3ba08
proph-050 <Josiahjoe321@gmail.com>
2023-07-20 19:52:51 -0700

    qwer

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Determine if a number is positive,negative or zero
+* 0 : is the number to be checked
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i=98 
+	positive_or_negative(i);
+
+return(0);
+}
